77e4b13a090fc62a15b8e49246eac5ad9a8b13ae
AVSHRUDAI <53274590+AVSHRUDAI@users.noreply.github.com>
2020-09-05 02:28:16 +0530

    commit1

--- app/src/main/java/com/example/zerohunger/MainActivity.java
+++ app/src/main/java/com/example/zerohunger/MainActivity.java
@@ -36,6 +36,48 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         toggle.syncState();
 
     }
+    private static final int TEZ_REQUEST_CODE = 123;
+    private static final String GOOGLE_TEZ_PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+
+      Button payButton = findViewById(R.id.pay_button);
+      payButton.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          Uri uri =
+              new Uri.Builder()
+                  .scheme("upi")
+                  .authority("pay")
+                  .appendQueryParameter("pa", "test@axisbank")
+                  .appendQueryParameter("pn", "Test Merchant")
+                  .appendQueryParameter("mc", "1234")
+                  .appendQueryParameter("tr", "123456789")
+                  .appendQueryParameter("tn", "test transaction note")
+                  .appendQueryParameter("am", "10.01")
+                  .appendQueryParameter("cu", "INR")
+                  .appendQueryParameter("url", "https://test.merchant.website")
+                  .build();
+          Intent intent = new Intent(Intent.ACTION_VIEW);
+          intent.setData(uri);
+          intent.setPackage(GOOGLE_TEZ_PACKAGE_NAME);
+          startActivityForResult(intent, TEZ_REQUEST_CODE);
+        }
+      });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+      super.onActivityResult(requestCode, resultCode, data);
+
+      if (requestCode == TEZ_REQUEST_CODE) {
+        // Process based on the data in response.
+        Log.d("result", data.getStringExtra("Status"));
+      }
+    }
 
     private void initView() {
 
