aa18471b70b077d72a823d9266d2c3e0f7a20800
Abhinay podugu <abhinaypodugu@gmail.com>
2020-09-04 13:12:15 +0530

    Added settings page , notifications, navigation bar, sharing

--- ZeroHungerLogin/app/src/main/java/com/example/zerohungerlogin/SettingsActivity.java
+++ ZeroHungerLogin/app/src/main/java/com/example/zerohungerlogin/SettingsActivity.java
@@ -1,24 +1,32 @@
 package com.example.zerohungerlogin;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.cardview.widget.CardView;
+
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
-
-public class SettingsActivity extends OptionsMenuActivity {
-Toolbar toolbar;
+public class SettingsActivity extends AppCompatActivity {
+TextView notifications,help,invitefriend;
+CardView cardView2,cardView3,cardView4;
+Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        setContentView(R.layout.activity_settings);
+        notifications = findViewById(R.id.notiTextView);
+        help = findViewById(R.id.helpTextView);
+        invitefriend = findViewById(R.id.inviteFriendTextView);
+        cardView2 = findViewById(R.id.cardview2);
+        cardView3 = findViewById(R.id.cardview3);
+        cardView4 = findViewById(R.id.cardview4);
 
 
 
     }
-
-
 }
--- ZeroHungerLogin/app/src/main/java/com/example/zerohungerlogin/StartActivity.java
+++ ZeroHungerLogin/app/src/main/java/com/example/zerohungerlogin/StartActivity.java
@@ -2,14 +2,17 @@ package com.example.zerohungerlogin;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.text.Html;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -25,6 +28,7 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
@@ -35,41 +39,67 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Random;
-import java.util.zip.Inflater;
+import java.util.zip.Inflater;import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class StartActivity extends OptionsMenuActivity {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-    FirebaseAuth fauth;
-    DatabaseReference reference;
-    Toolbar toolbar;
-    long p=600000;
+public class StartActivity extends OptionsMenuActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG="StartActivity";
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        //String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        toolbar =findViewById(R.id.toolbar);
+
+        initView();
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Homepage");
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.drawer_open,R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    private void initView() {
+
+        Log.d(TAG,"initView: started");
+        drawer =(DrawerLayout)findViewById(R.id.drawer);
+        navigationView =(NavigationView)findViewById(R.id.navigation_drawer);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.donatefood:
+                Toast.makeText(getApplicationContext(),"Item 1 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.fooddonors:
+                Toast.makeText(getApplicationContext(),"Item 2 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.moneydonors:
+                Toast.makeText(getApplicationContext(),"Item 3 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.moneyspent:
+                Toast.makeText(getApplicationContext(),"Item 4 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(),"Item 5 selected",Toast.LENGTH_SHORT).show();
+                return true;
         }
-
-        CountDownTimer cd = new CountDownTimer(p,2000) {
-            @Override
-            public void onTick(long l) {
-                p=l;
-                Random random = new Random();
-                int mColor = 0xff000000 | random.nextInt(0xffffff);
-                toolbar.setTitleTextColor(mColor);
-            }
-
-            @Override
-            public void onFinish() {
-
-            }
-        }.start();
+        return true;
     }
 
 
